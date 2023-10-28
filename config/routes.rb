887d9709e1f64caf510c4dfd8b51d04ff67257cf
vanthinh01052001 <vanthinh01052001@gmail.com>
2023-10-28 17:50:38 +0700

    change route from /users/sign_in to /login

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
   # root "posts#index"
 end
