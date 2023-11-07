63033cf7970d723a500b86ea17447cb09baf0853
vanthinh01052001 <vanthinh01052001@gmail.com>
2023-11-07 14:30:25 +0700

    Add CSS to the Posts new.html.erb template

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,29 @@ class PostsController < ApplicationController
     posts_for_branch(params[:action])
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to post_path(@post) 
+    else
+      redirect_to root_path
+    end
+  end
+
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+  
+  def get_posts
+    PostsForBranchService.new({
+      search: params[:search],
+      category: params[:category],
+      branch: params[:action]
+    }).call
+  end
+
   private
   def posts_for_branch(branch)
     @categories = Category.where(branch: branch)
@@ -26,30 +49,6 @@ class PostsController < ApplicationController
     end
   end
 
-  def get_posts
-    PostsForBranchService.new({
-      search: params[:search],
-      category: params[:category],
-      branch: params[:action]
-    }).call
-  end
-
-  def new
-    @branch = params[:branch]
-    @categories = Category.where(branch: @branch)
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    if @post.save 
-      redirect_to post_path(@post) 
-    else
-      redirect_to root_path
-    end
-  end
-
-  private
   def post_params
     params.require(:post).permit(:content, :title, :category_id)
                          .merge(user_id: current_user.id)
