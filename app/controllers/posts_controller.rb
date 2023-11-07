89fe66a50ac20a924a0ce8e168f891cec9c91d7d
vanthinh01052001 <vanthinh01052001@gmail.com>
2023-11-07 13:02:47 +0700

    Create a UI to create new posts - Inside the PostsController:   define new and create actions   define a post_params method   define a before_action filter - Inside the ApplicationController:   define a redirect_if_not_signed_in method   define a redirect_if_signed_in method - Create a new template for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :redirect_if_not_signed_in, only: [:new]
   def show
     @post = Post.find(params[:id])
   end
@@ -16,7 +17,6 @@ class PostsController < ApplicationController
   end
 
   private
-
   def posts_for_branch(branch)
     @categories = Category.where(branch: branch)
     @posts = get_posts.paginate(page: params[:page])
@@ -25,11 +25,33 @@ class PostsController < ApplicationController
       format.js { render partial: 'posts/posts_pagination_page' }
     end
   end
+
   def get_posts
     PostsForBranchService.new({
       search: params[:search],
       category: params[:category],
       branch: params[:action]
     }).call
+  end
+
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to post_path(@post) 
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content, :title, :category_id)
+                         .merge(user_id: current_user.id)
   end
 end
